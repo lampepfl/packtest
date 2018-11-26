3f1e11b2bc72ad0732de8dfb289ec4d8968d4846
Liu Fengyun <liu@fengy.me>
2018-11-26 11:32:27 +0100

    Fix call to bin/dotd in windows.bat

--- windows.bat
+++ windows.bat
@@ -4,4 +4,4 @@ CALL %0\..\bin\dotc.bat ..\examples\HelloWorld.scala
 CALL %0\..\bin\dotr.bat dotty.HelloWorld
 CALL %0\..\bin\dotr.bat -J-Xmx512m dotty.HelloWorld
 if not exist _site mkdir _site
-CALL %0\..\bin\dotd -siteroot _site -project Hello ..\examples\HelloWorld.scala
+CALL %0\..\bin\dotd.bat -siteroot _site -project Hello ..\examples\HelloWorld.scala
